20afcb9e6e5852e1dcfc4312d0a679f1c867ed82
Julien Romero <romerojulien34@gmail.com>
2019-07-10 00:38:39 +0200

    correct log message

--- quasimodo/identical_subject_object_submodule.py
+++ quasimodo/identical_subject_object_submodule.py
@@ -10,7 +10,7 @@ class IdenticalSubjectObjectSubmodule(SubmoduleInterface):
         self._name = "Identical subject/object"
 
     def process(self, input_interface):
-        logging.info("Start the antonym checking")
+        logging.info("Start identical subject/object removal")
         new_gfs = [g for g in input_interface.get_generated_facts()
                    if g.get_subject().get() != g.get_object().get()]
         return input_interface.replace_generated_facts(new_gfs)
