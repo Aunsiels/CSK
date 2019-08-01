58435a5b2046461af55d595e447d3886dfca89ed
Julien Romero <romerojulien34@gmail.com>
2019-08-01 19:19:22 +0200

    refractor fact combination

--- quasimodo/linear_combination_weighted_submodule.py
+++ quasimodo/linear_combination_weighted_submodule.py
@@ -1,21 +1,36 @@
 import logging
 import os
 
-from quasimodo.modality import Modality, get_multiple_parts_combination, read_sentence
-from .multiple_scores import MultipleScore
+from quasimodo.parts_of_facts import PartsOfFacts
 from .submodule_interface import SubmoduleInterface
-from .generated_fact import GeneratedFact
-from .multiple_module_reference import MultipleModuleReference
-from .multiple_submodule_reference import MultipleSubmoduleReference
-from .multiple_pattern import MultiplePattern
 from .trainer import Trainer
 from quasimodo.parameters_reader import ParametersReader
 
 
 save_weights = True
-save_file = os.path.dirname(__file__) + "/temp/weights.tsv"
+
+
 parameters_reader = ParametersReader()
 annotations_file = parameters_reader.get_parameter("annotations-file") or "data/all_manual_annotations.tsv"
+save_file = parameters_reader.get_parameter("weights-file") or os.path.dirname(__file__) + "/temp/weights.tsv"
+
+
+def _save_weights(parts_of_facts):
+    annotations = get_annotated_data()
+    header = parts_of_facts.get_header()
+    header.append("label")
+    save = ["\t".join(header)]
+    for fact in parts_of_facts.get_all_facts():
+        row = parts_of_facts.get_fact_row(fact)
+        row.append(annotations.get((fact.get_subject().get(),
+                                    fact.get_predicate().get(),
+                                    fact.get_object().get()),
+                                   -1))
+        row = [str(x) for x in row]
+        save.append("\t".join(row))
+    with open(save_file, "w") as f:
+        for element in save:
+            f.write(element + "\n")
 
 
 class LinearCombinationWeightedSubmodule(SubmoduleInterface):
@@ -24,182 +39,29 @@ class LinearCombinationWeightedSubmodule(SubmoduleInterface):
         super().__init__()
         self._module_reference = module_reference
         self._name = "Linear Combination Per Module Submodule"
-        self._weights = set()
-
-    def _get_fact_row(self, fact, d_gf, d_max, d_gf_modality, d_gf_patterns, d_gf_sentences):
-        names = self._weights
-        temp = [fact.get_subject().get(), fact.get_predicate().get(), fact.get_object().get()]
-        modality = d_gf_modality[fact]
-        if modality:
-            temp.append(modality)
-        else:
-            temp.append(" ")
-        if fact.is_negative():
-            temp.append(1)
-        else:
-            temp.append(0)
-        temp.append(d_gf_patterns[fact].to_str())
-        for name in names:
-            if name in d_max and d_max[name] == 0:
-                d_max[name] = 1
-            if name in d_gf[fact]:
-                if name in d_max:
-                    temp.append(d_gf[fact][name] /
-                                d_max[name])
-                else:
-                    temp.append(d_gf[fact][name])
-            else:
-                temp.append("")
-        temp.append(sum(d_gf_sentences[fact].values()))
-        return temp
-
-    def _save_weights(self, d_gf, d_max, d_gf_modality, d_gf_patterns, d_gf_sentences):
-        save = []
-        temp = []
-        annotations = get_annotated_data()
-        names = self._weights
-        temp.append("subject")
-        temp.append("predicate")
-        temp.append("object")
-        temp.append("modality")
-        temp.append("is negative")
-        temp.append("patterns")
-        for name in names:
-            temp.append(name)
-        temp.append("number sentences")
-        temp.append("label")
-        save.append("\t".join(temp))
-        for fact in d_gf:
-            row = self._get_fact_row(fact, d_gf, d_max, d_gf_modality, d_gf_patterns, d_gf_sentences)
-            row.append(annotations.get((fact.get_subject().get(),
-                                        fact.get_predicate().get(),
-                                        fact.get_object().get()),
-                                       -1))
-            row = [str(x) for x in row]
-            save.append("\t".join(row))
-        with open(save_file, "w") as f:
-            for element in save:
-                f.write(element + "\n")
 
     def process(self, input_interface):
         logging.info("Start linear combining per module submodule")
-        # group the tuples
-        d_gf = dict()
-        d_gf_sentences = dict()
-        d_gf_modules = dict()
-        d_gf_submodules = dict()
-        d_gf_patterns = dict()
-        d_gf_modality = dict()
-        df_valid = set()
+
         logging.info("Grouping facts")
-        for generated_fact in input_interface.get_generated_facts():
-            fact = generated_fact.get_fact()
-            modality = fact.get_modality()
-            fact = fact.change_modality("")
-            sentence_source = generated_fact.get_sentence_source()
-            for _, module_source, submodule_source in generated_fact.get_score().scores:
-                self._weights.add(submodule_source.get_name())
-            if modality.is_empty() or modality.contains_completing_part():
-                df_valid.add(fact)
-            if fact in d_gf:
-                for score, module_source, submodule_source in generated_fact.get_score().scores:
-                    if submodule_source.get_name() in d_gf[fact]:
-                        d_gf[fact][submodule_source.get_name()] += score
-                    else:
-                        d_gf[fact][submodule_source.get_name()] = score
-                    d_gf_modules[fact].add_reference(module_source)
-                    d_gf_submodules[fact].add_reference(submodule_source)
-                if len(sentence_source) > 0:
-                    for sentence, score in read_sentence(sentence_source):
-                        d_gf_sentences[fact][sentence] = d_gf_sentences[fact].get(sentence, 0) + score
-                if not modality.is_empty():
-                    for modality_temp, score in modality.get_modalities_and_scores():
-                        d_gf_modality[fact][modality_temp] = d_gf_modality[fact].get(modality_temp, 0) + score
-                d_gf_patterns[fact].add_pattern(generated_fact.get_pattern())
-            else:
-                d_gf[fact] = dict()
-                d_gf_modules[fact] = MultipleModuleReference(self._module_reference)
-                d_gf_submodules[fact] = MultipleSubmoduleReference(self)
-                for score, module_source, submodule_source in generated_fact.get_score().scores:
-                    d_gf[fact][submodule_source.get_name()] = score
-                    d_gf_modules[fact].add_reference(module_source)
-                    d_gf_submodules[fact].add_reference(submodule_source)
-                d_gf_sentences[fact] = dict()
-                d_gf_patterns[fact] = MultiplePattern(generated_fact.get_pattern())
-                d_gf_modality[fact] = dict()
-                if len(sentence_source) > 0:
-                    for sentence, score in read_sentence(sentence_source):
-                        d_gf_sentences[fact][sentence] = d_gf_sentences[fact].get(sentence, 0) + score
-                if not modality.is_empty():
-                    for modality_temp, score in modality.get_modalities_and_scores():
-                        d_gf_modality[fact][modality_temp] = d_gf_modality[fact].get(modality_temp, 0) + score
-        # Remove completely incomplete facts
-        to_delete = []
-        logging.info("Deleting " + str(len(to_delete)) + " TBC facts")
-        for fact in to_delete:
-            del d_gf[fact]
-            del d_gf_sentences[fact]
-            del d_gf_modules[fact]
-            del d_gf_submodules[fact]
-            del d_gf_patterns[fact]
-            del d_gf_modality[fact]
-        logging.info("Computing new sentences")
+        parts_of_facts = PartsOfFacts.from_generated_facts(input_interface.get_generated_facts())
 
-        # Compute the new modalities
-        logging.info("Computing new modalities")
-        for fact in d_gf_modality:
-            number_tbc = 0
-            total = 0
-            for modality_raw in d_gf_modality[fact]:
-                total += d_gf_modality[fact][modality_raw]
-                if "TBC" in modality_raw:
-                    number_tbc += d_gf_modality[fact][modality_raw]
-            d_gf[fact]["TBC"] = 0.0
-            if total > 0:
-                if total == number_tbc:
-                    d_gf[fact]["TBC"] = 1.0
-            d_gf_modality[fact] = Modality.from_modalities_and_scores(d_gf_modality[fact].items())
-        # Get the max of each name for normalization
-        logging.info("Computing max")
-        d_max = dict()
-        for fact in d_gf:
-            for name in d_gf[fact]:
-                if name in d_max:
-                    d_max[name] = max(d_gf[fact][name], d_max[name])
-                else:
-                    d_max[name] = d_gf[fact][name]
         if save_weights:
-            self._save_weights(d_gf, d_max, d_gf_modality, d_gf_patterns, d_gf_sentences)
+            logging.info("Saving weights facts")
+            _save_weights(parts_of_facts)
 
-        # Compute the scores
-        logging.info("Scoring the facts...")
-        d_scores = dict()
+        logging.info("Training the model...")
         trainer = Trainer(save_file)
         trainer.train()
-        for fact in d_gf:
-            row = self._get_fact_row(fact, d_gf, d_max, d_gf_modality, d_gf_patterns, d_gf_sentences)
-            d_scores[fact] = trainer.predict(fact, row)
 
-        # Compute the new sentences
-        for fact in d_gf_sentences:
-            d_gf_sentences[fact] = get_multiple_parts_combination(d_gf_sentences[fact].items())
-
-        # Transform to generated facts
+        logging.info("Generating new facts")
         new_generated_facts = []
-        for key in d_gf:
-            multiple_score = MultipleScore()
-            multiple_score.add_score(d_scores[key], d_gf_modules[key], d_gf_submodules[key])
-            new_generated_facts.append(GeneratedFact(
-                key.get_subject(),
-                key.get_predicate(),
-                key.get_object(),
-                d_gf_modality[key],
-                key.is_negative(),
-                multiple_score,
-                d_gf_sentences[key],
-                d_gf_patterns[key]))
+        for fact in parts_of_facts.get_all_facts():
+            new_generated_facts.append(parts_of_facts.get_generated_fact_with_score_from_classifier(fact, trainer))
+
         new_generated_facts = sorted(new_generated_facts,
                                      key=lambda x: -sum([score[0] for score in x.get_score().scores]))
+
         return input_interface.replace_generated_facts(new_generated_facts)
 
 
---
+++ quasimodo/parts_of_facts.py
@@ -0,0 +1,173 @@
+from quasimodo.generated_fact import GeneratedFact
+from quasimodo.modality import read_sentence, get_multiple_parts_combination, Modality
+from quasimodo.multiple_module_reference import MultipleModuleReference
+from quasimodo.multiple_pattern import MultiplePattern
+from quasimodo.multiple_scores import MultipleScore
+from quasimodo.multiple_submodule_reference import MultipleSubmoduleReference
+
+
+class PartsOfFacts(object):
+
+    def __init__(self):
+        self.found = dict()
+        self.sentences = dict()
+        self.modalities = dict()
+        self.patterns = dict()
+        self.modules = dict()
+        self.submodules = dict()
+        self.all_submodules = set()
+
+    def initialize_for_generated_fact(self, generated_fact):
+        fact_without_modality = get_fact_without_modality(generated_fact)
+        if fact_without_modality not in self.found:
+            self.found[fact_without_modality] = None
+            self.sentences[fact_without_modality] = dict()
+            self.modalities[fact_without_modality] = dict()
+            self.patterns[fact_without_modality] = MultiplePattern()
+            self.modules[fact_without_modality] = MultipleModuleReference()
+            self.submodules[fact_without_modality] = MultipleSubmoduleReference()
+
+    def add_score(self, generated_fact):
+        fact_without_modality = get_fact_without_modality(generated_fact)
+        if self.found[fact_without_modality] is None:
+            self.found[fact_without_modality] = generated_fact
+        else:
+            self.found[fact_without_modality].get_score().add(generated_fact.get_score())
+
+    def add_sentence_source(self, generated_fact):
+        fact_without_modality = get_fact_without_modality(generated_fact)
+        if len(generated_fact.get_sentence_source()) > 0:
+            for sentence, score in read_sentence(generated_fact.get_sentence_source()):
+                self.sentences[fact_without_modality][sentence] = \
+                    self.sentences[fact_without_modality].get(sentence, 0) + score
+
+    def add_modality(self, generated_fact):
+        fact = generated_fact.get_fact()
+        fact_without_modality = get_fact_without_modality(generated_fact)
+        if len(get_modality(fact)) > 0:
+            for modality_temp, score in read_sentence(get_modality(fact)):
+                self.modalities[fact_without_modality][modality_temp] = \
+                    self.modalities[fact_without_modality].get(modality_temp, 0) + score
+
+    def add_pattern(self, generated_fact):
+        fact_without_modality = get_fact_without_modality(generated_fact)
+        self.patterns[fact_without_modality].add_pattern(generated_fact.get_pattern())
+
+    def add_modules(self, generated_fact):
+        fact_without_modality = get_fact_without_modality(generated_fact)
+        for _, module_source, _ in generated_fact.get_score().scores:
+            self.modules[fact_without_modality].add_reference(module_source)
+
+    def add_submodules(self, generated_fact):
+        fact_without_modality = get_fact_without_modality(generated_fact)
+        for score, _, submodules_source in generated_fact.get_score().scores:
+            self.submodules[fact_without_modality].add_reference(submodules_source)
+            self.all_submodules.add(submodules_source)
+
+    def update(self, generated_fact):
+        self.initialize_for_generated_fact(generated_fact)
+        self.add_score(generated_fact)
+        self.add_sentence_source(generated_fact)
+        self.add_modality(generated_fact)
+        self.add_pattern(generated_fact)
+        self.add_modules(generated_fact)
+        self.add_submodules(generated_fact)
+
+    @staticmethod
+    def from_generated_facts(generated_facts):
+        parts_of_facts = PartsOfFacts()
+        for generated_fact in generated_facts:
+            parts_of_facts.update(generated_fact)
+        return parts_of_facts
+
+    def merge_into_generated_facts(self):
+        new_gfs = []
+        for fact_without_modality in self.found:
+            generated_fact = self.found[fact_without_modality]
+            new_sentence = get_multiple_parts_combination(self.sentences[fact_without_modality].items())
+            new_modality = Modality.from_modalities_and_scores(self.modalities[fact_without_modality].items())
+            new_gfs.append(generated_fact.change_sentence(new_sentence)
+                           .change_modality(new_modality)
+                           .change_pattern(self.patterns[fact_without_modality]))
+        return new_gfs
+
+    def get_all_submodules(self):
+        return self.all_submodules
+
+    def get_tbc_score(self, fact):
+        number_tbc = 0
+        total = 0
+        for modality_raw in self.modalities[fact]:
+            total += self.modalities[fact][modality_raw]
+            if "TBC" in modality_raw:
+                number_tbc += self.modalities[fact][modality_raw]
+        if total > 0:
+            if total == number_tbc:
+                return 1.0
+        return 0.0
+
+    def get_fact_row(self, fact):
+        result = [fact.get_subject().get(), fact.get_predicate().get(), fact.get_object().get()]
+        modality = Modality.from_modalities_and_scores(self.modalities[fact].items())
+        if modality:
+            result.append(modality)
+        else:
+            result.append(" ")
+        if fact.is_negative():
+            result.append(1)
+        else:
+            result.append(0)
+        result.append(self.patterns[fact].to_str())
+        names = self.get_all_submodules()
+        scores_per_submodules = dict()
+        for score, _, name in self.found[fact].get_score().scores:
+            if name in names:
+                scores_per_submodules[name] = score
+        for name in names:
+            if name in scores_per_submodules:
+                result.append(scores_per_submodules[name])
+            else:
+                result.append("")
+        result.append(sum(self.sentences[fact].values()))
+        return result
+
+    def get_header(self):
+        temp = ["subject", "predicate", "object", "modality", "is negative", "patterns"]
+        for submodule in self.get_all_submodules():
+            temp.append(submodule.get_name())
+        temp.append("number sentences")
+        return temp
+
+    def get_combined_sentence(self, fact):
+        return get_multiple_parts_combination(self.sentences[fact].items())
+
+    def get_generated_fact_with_score_from_classifier(self, fact, clf):
+        multiple_score = MultipleScore()
+        row = self.get_fact_row(fact)
+        score = clf.predict(fact, row)
+        multiple_score.add_score(score, self.modules[fact], self.submodules[fact])
+        return GeneratedFact(
+            fact.get_subject(),
+            fact.get_predicate(),
+            fact.get_object(),
+            self.modalities[fact],
+            fact.is_negative(),
+            multiple_score,
+            self.get_combined_sentence(fact),
+            self.patterns[fact])
+
+    def get_all_facts(self):
+        return self.found.keys()
+
+
+def get_fact_without_modality(generated_fact):
+    return generated_fact.get_fact().change_modality("")
+
+
+def get_modality(fact):
+    modality = fact.get_modality()
+    if modality is not None:
+        modality = modality.get()
+    else:
+        modality = ""
+    return modality
