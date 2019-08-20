26eee2e1d9bbd49ea56f66fa6312cd455450ed28
Julien Romero <romerojulien34@gmail.com>
2019-08-20 13:25:55 +0200

    more training data

--- quasimodo/tests/test_cleaning_predicate.py
+++ quasimodo/tests/test_cleaning_predicate.py
@@ -71,6 +71,15 @@ class TestCleaningPredicate(unittest.TestCase):
         generated_facts = inputs.get_generated_facts()
         self.assertEqual(1, len(generated_facts))
 
+    def test_not_digest(self):
+        generated_fact = GeneratedFact("elephant", "not digest", "fruits", "", False, 0.0, "")
+        inputs = self.empty_input.add_generated_facts([generated_fact])
+        inputs = self.cleaning_predicate.process(inputs)
+        generated_facts = inputs.get_generated_facts()
+        self.assertEqual(1, len(generated_facts))
+        self.assertEqual(generated_facts[0].get_predicate().get(), "digest")
+        self.assertTrue(generated_facts[0].is_negative())
+
 
 if __name__ == '__main__':
     unittest.main()
